da553f36474ec3744ba6c9e1d8c494a63a554d3a
Aabah94 <47247251+Aabah94@users.noreply.github.com>
2019-06-20 17:19:11 +0530

    Update TestNgMavenSecondClass.java

--- src/test/java/com/javacodegeeks/testng/maven/TestNgMavenSecondClass.java
+++ src/test/java/com/javacodegeeks/testng/maven/TestNgMavenSecondClass.java
@@ -3,9 +3,9 @@ package com.javacodegeeks.testng.maven;
 import org.testng.annotations.Test;
 import com.agiletestware.bumblebee.annotations.BooleanValue;
 import com.agiletestware.bumblebee.annotations.Bumblebee;
-
-public class TestNgMavenSecondClass {
 @Bumblebee(testname= "Tesng code", testlab = "Root\\TestBumblebee", testset = "TestBumblebee", testplan = "Subject\\TestBumblebee")
+public class TestNgMavenSecondClass {
+	@Bumblebee(testname = "Tesng code")
 	@Test
 	public void oneMoreTest() {
 		System.out.println("This is a TestNG-Maven based test");
