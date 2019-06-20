22bde84e0744d1862b2a118c000bb774417ef586
Aabah94 <47247251+Aabah94@users.noreply.github.com>
2019-06-20 17:18:12 +0530

    Update TestNgMavenExample.java

--- src/test/java/com/javacodegeeks/testng/maven/TestNgMavenExample.java
+++ src/test/java/com/javacodegeeks/testng/maven/TestNgMavenExample.java
@@ -4,9 +4,10 @@ import org.testng.annotations.Test;
 import com.agiletestware.bumblebee.annotations.BooleanValue;
 import com.agiletestware.bumblebee.annotations.Bumblebee;
 
-
-public class TestNgMavenExample {
 @Bumblebee(testname= "Tesng code", testlab = "Root\\TestBumblebee", testset = "TestBumblebee", testplan = "Subject\\TestBumblebee")
+public class TestNgMavenExample {
+
+	@Bumblebee(testname = "Tesng code")
 	@Test
 	public void exampleOfTestNgMaven() {
 		System.out.println("This is TestNG-Maven Example");
